2d3c3a5bf25af3de19c79ee67d1fb5c25a471fbe
huifeidelang <346749931@qq.com>
2016-08-10 19:22:28 +0800

    fanCh
    fanCh

---
+++ config.h
@@ -0,0 +1,18 @@
+
+/* #undef HAVE_UNISTD_H */
+#define HAVE_STDLIB_H
+/* #undef HAVE_SYS_WAIT_H */
+/* #undef HAVE_TERMIOS_H */
+/* #undef HAVE_PTY_H */
+/* #undef HAVE_UTMP_H */
+#define HAVE_FCNTL_H
+#define HAVE_STRING_H
+/* #undef HAVE_OPENPTY */
+/* #undef HAVE__GETPTY */
+/* #undef HAVE_DEV_PTMX */
+/* #undef HAVE_DEV_PTS */
+/* #undef HAVE_LOGIN_TTY */
+
+#define VERSION     "3.4.0"
+#define PACKAGE     "fgrun"
+#define LOCALEDIR   "C:/Program Files (x86)/fgrun/share/locale"
